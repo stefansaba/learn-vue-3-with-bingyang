8307bfa401de8f6a87768ae43a343544ae9065df
Stefan Saba <stefan.saba@icloud.com>
2025-02-23 16:26:42 -0600

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,109 @@
 <template>
   <h1>{{ message }}</h1>
-
-  <!-- replaceText is the name of a function defined in the script -->
-  <button v-on:click="replaceText('v-on is fun!')">Replace text</button>
-
-  <!-- v-on:click shortens to @click -->
-  <button @click="replaceText('v-on is fun!')">Replace text</button>
-
-  <hr />
-
-  <!-- using $event special variable to access native DOM event -->
-  <p>An input field where the user can ONLY enter numbers:</p>
-  <input type="text" @keydown="handleInput($event)" />
-
-  <hr />
-  <p>A demo of preventing the default behavior of an event.</p>
-  <p>Right click in the cyan box below will not show the context menu:</p>
-  <div
-    style="width: 100px; height: 100px; background-color: aqua"
-    @contextmenu.prevent="console.log('Show a custom context menu instead.')"
-  ></div>
-
-  <hr />
-
-  <p>A demo of stopping event propagation:</p>
-
-  <div id="mouseover" @mouseover="fun1">
-    <textarea @mouseover.stop="fun2($event)">This is a text area.</textarea>
-  </div>
-  <!-- Since textarea is nested within div, mouseover textarea also means mouseover the parent div. -->
-
-  <hr />
-  <div>
-    Press down the "Enter" key will trigger a console log print:
-    <input
-      type="text"
-      @keydown.enter="console.log('You pressed the Enter key.')"
-    />
-  </div>
-  <div>
-    Press down the "Arrow Down" key will trigger a console log print:
-    <input
-      type="text"
-      @keydown.down="console.log('You pressed the Arrow Down key.')"
-    />
-  </div>
-  <div>
-    Press down the "Space" key will trigger a console log print:
-    <input
-      type="text"
-      @keydown.space="console.log('You pressed the Space key.')"
-    />
-  </div>
-  <div>
-    Press down the "b" key will trigger a console log print:
-    <input type="text" @keydown.b="console.log('You pressed the B key.')" />
-  </div>
-  <div>
-    Press down the "Ctrl c" will trigger a console log print:
-    <input type="text" @keydown.ctrl.c="console.log('You pressed Ctrl c.')" />
-  </div>
+  <h1>Welcome, {{ formData.name }}</h1>
+  <form action="">
+    <div>
+      <span>Name:</span>
+      <span>
+        <!-- formData.name is synced after "change" event instead of "input" event. -->
+        <input type="text" v-model.lazy="formData.name" />
+      </span>
+    </div>
+    <div>
+      <span>Gender:</span>
+      <span>
+        <input type="radio" id="male" value="M" v-model="formData.gender" />
+        <label for="male">M</label>
+        <input type="radio" id="female" value="F" v-model="formData.gender" />
+        <label for="female">F</label>
+      </span>
+    </div>
+    <div>
+      <span>Age:</span>
+      <span>
+        <!-- Input is automatically typecast as a number. -->
+        <input type="text" id="age" v-model.number="formData.age" />
+      </span>
+    </div>
+    <div>
+      <span>Hobbies:</span>
+      <!-- We can bind multiple checkboxes to the same array. -->
+      <input type="checkbox" value="basketball" v-model="formData.hobbies" />
+      <label for="basketball">Basketball</label>
+      <input type="checkbox" value="football" v-model="formData.hobbies" />
+      <label for="football">Football</label>
+      <input type="checkbox" value="js" v-model="formData.hobbies" />
+      <label for="js">JavaScript</label>
+    </div>
+    <div>
+      <span>Profession:</span>
+      <select v-model="formData.profession">
+        <option value="">Please select one</option>
+        <option value="1">Software Engineer</option>
+        <option value="2">Data Scientist</option>
+        <option value="3">Product Manager</option>
+        <option value="4">Professor</option>
+      </select>
+    </div>
+    <div>
+      <span>Description:</span>
+      <!-- Leading and trailing whitespace is removed automatically. -->
+      <textarea
+        v-model.trim="formData.desc"
+        @keyup.enter="submitForm"
+      ></textarea>
+    </div>
+    <div>
+      <input type="reset" value="Reset" @click="resetForm" />
+      <!-- Prevent the form from submitting through form action. -->
+      <input type="submit" value="Submit" @click.prevent="submitForm" />
+    </div>
+  </form>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-let message = ref('Hello, v-on!')
+let message = ref('Hello, v-model!')
 
-function replaceText(msg) {
-  message.value = msg
-}
+let formData = ref({
+  name: '',
+  gender: '',
+  age: '',
+  hobbies: [],
+  profession: '',
+  desc: ''
+})
 
-function handleInput(event) {
-  // get the key code of the key pressed by user
-  let keyCode = event.keyCode
-  // if the key pressed is not a number key (0 - 9), don't show in the input box
-  if (keyCode < 48 || keyCode > 57) {
-    // vanilla JS code:
-    event.preventDefault()
+function resetForm() {
+  formData.value = {
+    name: '',
+    gender: '',
+    age: '',
+    hobbies: [],
+    profession: '',
+    desc: ''
   }
 }
 
-function fun1() {
-  console.log('mouse over div')
-}
-
-function fun2(event) {
-  console.log('mouse over textarea')
+function submitForm() {
+  console.log(JSON.stringify(formData.value))
+  // We can use axios or fetch() to submit data to the back end.
 }
 </script>
 
 <style scoped>
-#mouseover {
-  text-align: right;
-  background-color: purple;
-  width: 300px;
-  height: 300px;
+form {
+  padding: 20px;
+}
+
+form div {
+  height: 40px;
+  line-height: 40px;
+}
+
+form div span {
+  display: inline-block;
+  width: 100px;
 }
 </style>
