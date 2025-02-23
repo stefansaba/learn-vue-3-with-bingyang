0fbc72cbb9825e7b73b4a27b2539f9e986aba28b
Stefan Saba <stefan.saba@icloud.com>
2025-02-23 16:28:52 -0600

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <h1>{{ message }}</h1>
-  <p>
-    What has keys but can't open locks, space but no room, and you can enter but
-    can't go outside? What am I?
-  </p>
+  <button @click="sortUsersByAge">Sort users by age</button>
+  <ul>
+    <li v-for="(user, index) in users" :key="user.id">
+      {{ index }} - {{ user.id }} - {{ user.name }} - {{ user.age }} -
+      {{ user.isActive }}
+    </li>
+  </ul>
 
-  <button style="width: 200px" @click="showAnswer = !showAnswer">
-    {{ !showAnswer ? 'Show me the answer!' : 'Hide the answer!' }}
-  </button>
+  <hr />
 
-  <div v-if="showAnswer">A keyboard.</div>
+  <table>
+    <tr>
+      <th>Index</th>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Age</th>
+      <th>Operation</th>
+    </tr>
+    <tr v-for="(user, index) in users" :key="user.id">
+      <td>{{ index }}</td>
+      <td>{{ user.id }}</td>
+      <td :class="{ inactive: !user.isActive }">{{ user.name }}</td>
+      <td>{{ user.age }}</td>
+      <td>
+        <button @click="user.isActive = !user.isActive">
+          {{ user.isActive ? 'Deactivate' : 'Restore' }}
+        </button>
+      </td>
+    </tr>
+  </table>
 
-  <div v-show="showAnswer">A keyboard.</div>
-
-  <!-- <div v-if="Math.random() > 0.5">Now you see me</div>
-  <div v-else>Now you don't</div> -->
+  <hr />
+  <p>Use v-for to iterate through the properties of an object:</p>
+  <div v-for="(value, key, index) in users[0]">
+    {{ index }} - {{ key }}: {{ value }}
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
-let message = ref('Hello, v-if!')
+let message = ref('Hello, v-for!')
 
-let showAnswer = ref(false)
+const users = ref([
+  { id: 1001, name: 'John Smith', age: 26, isActive: false },
+  { id: 1002, name: 'Tom Doe', age: 16, isActive: false },
+  { id: 1003, name: 'Frankin Wong', age: 18, isActive: true }
+])
+
+function sortUsersByAge() {
+  users.value.sort((a, b) => a.age - b.age)
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+</style>
